3f0e8fa14c2c305b39530b631310ec4c2bc42985
jax2net <jacob@jax2.net>
2022-04-24 14:32:51 -0700

    Starter input code

--- server/server.js
+++ server/server.js
@@ -6,16 +6,15 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-    res.send('HI FORTNITE');
-    io.emit('con', 'I love fortnite.');
+    io.emit('con', 'I love fortnite.'); // initial connection message, not necessary
 });
 
 io.on('connection', (socket) => {
     console.log('A client has connected');
     io.emit('con', 'You have connected to the WS Server.');
-    socket.on('response', msg => {
-        io.emit('Response from server: ', "The 21st nite of september?");
-        console.log('Response from Client: ', msg);
+    socket.on('jump', input => {
+        io.emit('SERVER: You have pressed jump.');
+        console.log('Response from Client: ', input);
     });
     socket.on('disconnect', () => {
         console.log('Disconnected.');
